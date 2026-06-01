51c50a5280cbacf9043fa420af48f00e413dc164
Ana Oliveira <86444654+AnaOliveira100@users.noreply.github.com>
2026-06-01 11:03:00 -0300

    exercicios html css

---
+++ 05-condicionais.js
@@ -0,0 +1,19 @@
+'use strict';
+/*operadores RELACIONAIS USADDOR PARA REALIZAR COMPARAÇOES LOGICOAS
+==(IGUALDADE POR VALOR), ===(IGUALDADE POR VALOR E TIPO)
+< MENOR, > MAIOR, ,=MEMNOR OU IGUAL >= MAIOR OU IGUAL
+!= DIFERENÇAS POR VALOR  !== DIFERENÇA POR VALOR E TIPO */
+
+let a = 10;
+let b = 10;
+let c = 100;
+let d= "100"; // valor cpom string/texto
+
+console.log(a > b); //false a nao e mairo que b
+console.log(a >= b); //true a e iguyal as b
+console.log(c != a); //true c e diferente de a
+//comparando apenas valor usando ==
+console.log(c == d); // true valor sao os mesmos
+console.log(c === d); //false posi tipo de dado e diferente
+
+
